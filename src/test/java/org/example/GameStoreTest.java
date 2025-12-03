9fa0966548730d453dc77bb63ce9acc7861b2448
SuslovQA <evgenijsuslov08683@gmail.com>
2025-12-03 12:11:14 +0500

    add tests in project

--- src/test/java/org/example/GameStoreTest.java
+++ src/test/java/org/example/GameStoreTest.java
@@ -15,6 +15,16 @@ public class GameStoreTest {
         store.addPlayTime("Don", 6);
     }
 
+
+    @Test
+    public void shouldContainsGame() {
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("1 Баттл Онлайн", "Шутер");
+        Game game3 = store.publishGame("2 Баттл Онлайн", "Стратегии");
+
+        assertTrue(store.containsGame(game3));
+    }
+
     @Test
     public void shouldAddGame() {
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
@@ -23,8 +33,18 @@ public class GameStoreTest {
     }
 
     @Test
+    public void shouldNotAddGame() {
+        Game game = null;
+
+        assertFalse(store.containsGame(game));
+    }
+
+
+    @Test
     public void shouldAddPlayTime() {
-        int expected = 5;
+        store.addPlayTime("Nick", 1);
+
+        int expected = 6;
         int actual = store.playedTime.get("Nick");
 
         Assertions.assertEquals(expected, actual);
